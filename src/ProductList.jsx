40f480c64f2e18e40ae8ae14830fb6bc0b29f772
David Amaya <amayadavid885@gmail.com>
2024-11-10 19:49:05 -0500

    intial task 1 & 2 completed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,13 @@
 import React, { useState,useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -246,6 +250,14 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+  const handleAddToCart = (plant) => {
+    dispatch(addItem(plant));
+    setAddedToCart((prevState) => ({
+     ...prevState,
+     [product.name]: true,
+   }));
+  }
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -268,6 +280,23 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+
+            {plantsArray.map((item, index) => {
+                <div key={index}>
+                    <h1><div>{item.category}</div></h1>
+                    <div className="product-list">
+                        {item.plants.map((plant, pIndex) => {
+                            <div className="product-card" key={pIndex}>
+                                <img className="product-image" src={plant.image} alt={plant.name} />
+                                <div className="product-title">{plant.name}</div>
+                                <div className="product-title">{plant.description}</div>
+                                <div className="product-price">{plant.cost}</div>
+                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                        })}
+                        </div>
+                </div>
+            })}
 
 
         </div>
